e221e1365ce419c454d029bb3c8830e25e48ff53
M-Ali-Haider <mah01134212100@gmail.com>
2024-01-07 21:46:47 +0500

    debugging

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'https://ytdb-deploy.vercel.app'
+      '/api':'https://ytdb-deploy.vercel.app/'
     },
   },
   plugins: [react()],
