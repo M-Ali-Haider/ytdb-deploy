54d57b070af98ac52935f899f0a92759c7ff04c7
M-Ali-Haider <mah01134212100@gmail.com>
2024-01-07 21:23:07 +0500

    made changes to index.js

--- server/index.js
+++ server/index.js
@@ -1,54 +1,39 @@
+
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
 import mongoose from "mongoose";
+
 import dotenv from "dotenv";
 import userRoutes from "./routes/users.js";
 import videoRoutes from "./routes/videos.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
-import cookieParser from "cookie-parser";
-import cors from "cors"
 
 const app = express();
 dotenv.config();
 
-const connect = () => {
-  mongoose
-    .connect(process.env.MONGO)
-    .then(() => {
-      console.log("Connected to DB");
-    })
-    .catch((err) => {
-      console.error("Error connecting to DB:", err.message);
-    });
-};
-
-
-//middlewares
-app.use(cors({
-  origin: "https://ytdb-deploy-api.vercel.app",
-  credentials: true,
-}));
+app.use(cors())
+app.use(express.json())
 app.use(cookieParser())
-app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/videos", videoRoutes);
 app.use("/api/comments", commentRoutes);
 
-//error handler
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || "Something went wrong!";
-  return res.status(status).json({
-    success: false,
-    status,
-    message,
-  });
-});
+const port = process.env.PORT || 8800;
 
-const port = process.env.PORT || 8800
+const server = http.createServer(app)
 
-app.listen(port, () => {
-  connect();
-  console.log("Connected to Server");
-});
+mongoose.connect(process.env.MONGO)
+    .then(()=>{
+        console.log("MongoDB Connected!")
+        server.listen(port,()=>{
+            console.log(`Server is listening on port ${port} `)
+        })
+        .catch((err)=>{
+            console.log({err});
+        })
+    })
