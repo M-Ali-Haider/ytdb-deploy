e64c1ce99252f147952b3c99e8732a7cfd7a61b5
M-Ali-Haider <mah01134212100@gmail.com>
2024-01-07 20:14:09 +0500

    added cors

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import videoRoutes from "./routes/videos.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors"
 
 const app = express();
 dotenv.config();
@@ -24,6 +24,7 @@ const connect = () => {
 
 
 //middlewares
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json());
 app.use("/api/auth", authRoutes);
