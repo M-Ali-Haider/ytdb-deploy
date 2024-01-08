01658327669613613ddc074f6057a638ed4aa590
Muhammad Ali Haider <136558067+M-Ali-Haider@users.noreply.github.com>
2024-01-09 00:02:03 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 dotenv.config();
 
 const corsOptions = {
-    origin: "https://ytdb-deploy-frontend.vercel.app/",
+    origin: "https://ytdb-deploy-frontend.vercel.app",
     credentials: true, 
   };
 
