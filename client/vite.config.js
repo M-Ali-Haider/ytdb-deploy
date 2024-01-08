8866de0dee6035283e0b48cc07f9a63eb39bd7af
Muhammad Ali Haider <136558067+M-Ali-Haider@users.noreply.github.com>
2024-01-08 23:54:15 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:'/ytdb-deploy-frontend.vercel.app/'
+  base:'/ytdb-deploy-frontend.vercel.app/',
   server:{
     proxy:{
       '/api':'https://ytdb-deploy.vercel.app/api/'
