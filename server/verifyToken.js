5bca8b2fdca19fab9ef1440b1d40d7ed363c23f3
M-Ali-Haider <mah01134212100@gmail.com>
2024-01-09 22:50:26 +0500

    bruh

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -4,9 +4,15 @@ import { createError } from "./error.js";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) return next(createError(401, "You are not authenticated!"));
+
   Jwt.verify(token, process.env.JWT, (err, user) => {
-    if (err) return next(createError(403, "Token is not valid!"));
+    if (err) {
+      console.error("Token verification error:", err);
+      return next(createError(403, "Token is not valid!"));
+    }
+
+    console.log("Token verified successfully. User:", user);
     req.user = user;
-    next()
+    next();
   });
 };
